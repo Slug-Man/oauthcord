594d9da7dd2f6202fec4e1bdf0d2f55192fd8f4f
Logan Webb <lganwebb04@gmail.com>
2019-04-18 21:54:30 +0100

    Fixing repo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as ld:
     
 setup(
     name="oauthcord",
-    version="0.2.0",
+    version="0.2.1",
     author="Logan Webb",
     author_email="lganWebb04@gmail.com",
     description="An Oauth2 wrapper for discord",
